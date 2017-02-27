c011fcb93ecfaa2f1720a3d9edf1b868842bcf49
Natalia Rozmarynovych <Natalia Rozmarynovych>
2017-02-28 00:31:47 +0200

    Fixed functionality of Save button while pressing Change button

--- 10/phonebook/app/controllers/myCtrl.js
+++ 10/phonebook/app/controllers/myCtrl.js
@@ -2,25 +2,29 @@ angular.module('Controllers', ["taskService"]).controller('mainCtrl', mainCtrl)
 mainCtrl.$inject = ["myService"];
 
 function mainCtrl(myService) {
+    var changeIndex;
+    var isAdd = false;
     this.show = false;
     this.list = myService.list;
     this.addNew = function() {
         this.show = true;
+        isAdd = true;
         // myService.addNew(name, surname, number);
     };
     this.saveNew = function(x, y, z) {
-        // if (addNew) {
+        if (isAdd == true) {
             this.list.push({
                 name: x,
                 surname: y,
                 number: z
             });
-            // if (change) {
-            //     this.newname = this.list[index].name;
-            //     this.newsurname = this.list[index].surname;
-            //     this.newnumber = this.list[index].number;
-            // }
-        // }
+            isAdd = false;
+        } else {
+            this.list[changeIndex].name = this.newname;
+            this.list[changeIndex].surname = this.newsurname;
+            this.list[changeIndex].number = this.newnumber;
+        }
+
         this.show = false;
         this.newname = "";
         this.newsurname = "";
@@ -28,6 +32,7 @@ function mainCtrl(myService) {
     };
     this.change = function(index) {
         this.show = true;
+        changeIndex = index;
         this.newname = this.list[index].name;
         this.newsurname = this.list[index].surname;
         this.newnumber = this.list[index].number;
